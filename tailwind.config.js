b165884bd16e4d98153abdfe6bab2228706b6a35
Andrew kanyinji <kanyinjiandy2@gmail.com>
2024-08-21 00:01:32 +0200

    configure tailwind css for easy styling with its utility classes

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./pages/**/*.html",
+    "./pages/services/**/*.html",
+    "./assets/src/css/**/*.css",
+    "./assets/src/js/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
